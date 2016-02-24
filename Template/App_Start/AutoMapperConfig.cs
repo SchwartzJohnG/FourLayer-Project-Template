2a3506c9610a376b89548d98ed9adbaeb69842c3
John Schwartz <SchwartzJohnG@Gmail.com>
2016-02-23 19:04:28 -0600

    Added files via upload

---
+++ Template/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Template.Models;
+using Template.Services.Models;
+
+namespace Template.App_Start {
+    public class AutoMapperConfig {
+
+        public static void RegisterMaps() {
+            Mapper.CreateMap<ApplicationUserDTO, ApplicationUser>();
+            Mapper.CreateMap<ApplicationUser, ApplicationUserDTO>();
+        }
+    }
+}
